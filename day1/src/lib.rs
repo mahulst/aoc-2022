76a8ec19c23a3d6d27ab8bf74b91bb6d4de000fa
mahulst <michel@voorkanter.com>
2022-12-03 14:11:51 +0100

    Day 1

--- day1/src/lib.rs
+++ day1/src/lib.rs
@@ -1,9 +1,24 @@
 pub fn run_part_1(input: &str) -> String {
-    return "works".to_string();
+    let elf_with_heaviest_load = input.split("\n\n").map(|single_elf| {
+        return single_elf.lines().map(|item| {
+            item.trim().parse::<usize>().unwrap()
+        }).sum::<usize>();
+    }).max().unwrap();
+
+    return elf_with_heaviest_load.to_string();
 }
 
 pub fn run_part_2(input: &str) -> String {
-    return "works".to_string();
+    let mut three_elves: Vec<usize> = input.split("\n\n").map(|single_elf| {
+        return single_elf.lines().map(|item| {
+            item.trim().parse::<usize>().unwrap()
+        }).sum::<usize>();
+    }).collect();
+
+    three_elves.sort();
+    three_elves.reverse();
+
+    return three_elves.iter().take(3).sum::<usize>().to_string();
 }
 
 #[cfg(test)]
@@ -11,8 +26,42 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = run_part_1("hello");
-        assert_eq!(result, "works");
+    fn test_part_1_works() {
+        let input = "1000
+        2000
+        3000
+
+        4000
+
+        5000
+        6000
+
+        7000
+        8000
+        9000
+
+        10000";
+        let result = run_part_1(input);
+        assert_eq!(result, "24000");
+    }
+
+    #[test]
+    fn test_part_2_works() {
+        let input = "1000
+        2000
+        3000
+
+        4000
+
+        5000
+        6000
+
+        7000
+        8000
+        9000
+
+        10000";
+        let result = run_part_2(input);
+        assert_eq!(result, "45000");
     }
 }
