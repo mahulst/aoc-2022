652a6b1b0e30cbdea88b280dedff19a53078e6da
mahulst <michel@voorkanter.com>
2022-12-07 19:45:04 +0100

    Day 6

--- day6/src/lib.rs
+++ day6/src/lib.rs
@@ -1,9 +1,36 @@
+fn unique(s: &[char]) -> Option<(usize, usize, char)> {
+    s.iter().map(|a| a.clone()).enumerate().find_map(|(i, c)| {
+        s.iter().map(|a| a.clone()).enumerate()
+            .skip(i + 1)
+            .find(|(_, other)| c == *other)
+            .map(|(j, _)| (i, j, c))
+    })
+}
+
 pub fn run_part_1(input: &str) -> String {
-    return "works".to_string();
+    let chars: Vec<char> = input.chars().collect();
+    let mut count = 4;
+    for w in chars.windows(4) {
+        if unique(&w).is_none() {
+            return count.to_string();
+        } else {
+            count += 1;
+        }
+    }
+    return "0".to_string();
 }
 
 pub fn run_part_2(input: &str) -> String {
-    return "works".to_string();
+    let chars: Vec<char> = input.chars().collect();
+    let mut count = 14;
+    for w in chars.windows(14) {
+        if unique(&w).is_none() {
+            return count.to_string();
+        } else {
+            count += 1;
+        }
+    }
+    return "0".to_string();
 }
 
 #[cfg(test)]
@@ -12,7 +39,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = run_part_1("hello");
-        assert_eq!(result, "works");
+        let result = run_part_1("mjqjpqmgbljsphdztnvjfqwrcgsmlb");
+        assert_eq!(result, "7");
     }
 }
